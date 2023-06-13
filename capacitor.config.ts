7bb6a3582bb80e2c4da161294d90ea466d7e96ef
hugo.vanderzalm <hugozalm@merkator.com>
2023-06-13 14:06:16 +0200

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'building-analyser',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
